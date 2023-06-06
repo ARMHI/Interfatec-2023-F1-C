96e92d4e70adc449d302a01c99c281b105ad3008
Artur.M.H.I <117451939+ARMHI@users.noreply.github.com>
2023-06-06 14:40:17 -0300

    Update A.c

--- A.c
+++ A.c
@@ -1,1 +1,59 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+int main ( ){
+
+
+    char *last_order_books, temp;
+    int n_books, counter, *initial_position, *distance_from_initial_position, clock, click, times_book_was_read = 0;
+
+    scanf (" %d\n", &n_books );
+    last_order_books = (char*) malloc ( sizeof( char)* n_books);
+    distance_from_initial_position = (int*) malloc ( sizeof(int)* n_books);
+    initial_position = (int*) malloc ( sizeof(int)* n_books);
+
+
+    fgets( last_order_books, n_books +1, stdin );
+
+    for ( clock = 0; clock < n_books; clock ++){
+
+        counter = 0;
+        for( click = 0; click < n_books; click++){
+            if ( last_order_books[clock] > last_order_books[click] ){counter++;}
+        }
+    *(initial_position + clock) = counter;
+    *(distance_from_initial_position + clock) = initial_position[clock] - clock;
+
+
+
+    }
+    
+
+    for (click = 0; click < n_books - 1; click++)
+    {
+    for (clock = 0; clock < n_books - 1; clock++)
+    {
+
+            if (*(last_order_books + clock) > *(last_order_books + clock + 1))
+            {
+                temp = *(last_order_books + clock);
+                *(last_order_books + clock) = *(last_order_books + clock + 1);
+                *(last_order_books + clock + 1) = temp;
+                times_book_was_read++;
+            }
+    }
+    }
+
+
+    if (times_book_was_read > 5){ printf ("A Database Systems student read a book.\n");}
+    else{"%d\n", times_book_was_read;}
+
+    
+    
+
+free (last_order_books);
+free (distance_from_initial_position);
+free (initial_position);
+return 0;
+
+}
